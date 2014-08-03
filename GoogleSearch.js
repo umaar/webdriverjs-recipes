2f7dcf6338a23dc7109ab2e7e427fd6244a99a78
Umar <umar.hansa@gmail.com>
2014-08-03 20:20:23 +0100

    be fluent

--- GoogleSearch.js
+++ GoogleSearch.js
@@ -7,10 +7,6 @@
 var webdriver = require('selenium-webdriver');
 var browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'chrome' }).build();
 
-browser.get('https://www.google.com');
-browser.findElement(webdriver.By.name('q')).sendKeys('tuts+ code');
-browser.findElement(webdriver.By.name('btnG')).click();
-
 function logTitle() {
 	browser.getTitle().then(function(title) {
 		console.log('Current Page Title: ' + title);
@@ -20,11 +16,10 @@ function logTitle() {
 
 function clickLink(link) {
 	link.click();
-	logTitle();
 }
 
 function handleFailure(err) {
-	console.error(err)
+	console.error(err.stack)
 }
 
 function findTutsPlusLink() {
@@ -33,8 +28,7 @@ function findTutsPlusLink() {
 	});
 }
 
-browser.wait(findTutsPlusLink, 2000).then(clickLink, handleFailure);
-
-
-
-
+browser.get('https://www.google.com');
+browser.findElement(webdriver.By.name('q')).sendKeys('tuts+ code');
+browser.findElement(webdriver.By.name('btnG')).click();
+browser.wait(findTutsPlusLink, 2000).then(clickLink).then(logTitle, handleFailure);
